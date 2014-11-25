e496d8208b68b2bc000dd1f012b380b757178258
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2014-11-24 18:03:40 -0600

    Factored out combinator from withType calls

--- src/lambda.js
+++ src/lambda.js
@@ -7,9 +7,7 @@ function id(x){
   return x;
 }
 function κ(x){
-  return function(){
-    return x;
-  };
+  return function(){ return x; };
 }
 function η(f){
   return function(x){ return f(x); };
@@ -42,10 +40,10 @@ function annotate(τ, f){
 //TODO: figure out how to decompose this with combinators
 function withType(τ, f){
   return function(k){
-    return annotate(τ, function(y){
-      return function(x){
+    return annotate(κ(τ), function(y){
+      return annotate(τ, function(x){
         return f(k(x), y);
-      };
+      });
     });
   };
 }
@@ -104,41 +102,41 @@ var andThen = function(k){
 //methods
 τ.any.andThen              = andThen;
 
-τ.any.toString             = withType(κ(τ.string), prim.any.toString             );
-τ.any.toLocaleString       = withType(κ(τ.string), prim.any.toLocaleString       );
+τ.any.toString             = withType(τ.string, prim.any.toString             );
+τ.any.toLocaleString       = withType(τ.string, prim.any.toLocaleString       );
 //operators
-τ.any.equalTo              = withType(κ(τ.bool),   prim.any.equalTo              );
-τ.any.notEqualTo           = withType(κ(τ.bool),   prim.any.notEqualTo           );
-τ.any.lessThan             = withType(κ(τ.bool),   prim.any.lessThan             );
-τ.any.greaterThan          = withType(κ(τ.bool),   prim.any.greaterThan          );
-τ.any.greaterThanOrEqualTo = withType(κ(τ.bool),   prim.any.greaterThanOrEqualTo );
-τ.any.lessThanOrEqualTo    = withType(κ(τ.bool),   prim.any.lessThanOrEqualTo    );
+τ.any.equalTo              = withType(τ.bool,   prim.any.equalTo              );
+τ.any.notEqualTo           = withType(τ.bool,   prim.any.notEqualTo           );
+τ.any.lessThan             = withType(τ.bool,   prim.any.lessThan             );
+τ.any.greaterThan          = withType(τ.bool,   prim.any.greaterThan          );
+τ.any.greaterThanOrEqualTo = withType(τ.bool,   prim.any.greaterThanOrEqualTo );
+τ.any.lessThanOrEqualTo    = withType(τ.bool,   prim.any.lessThanOrEqualTo    );
 
 
 //-----------------------------------------------------------------------------
 // Array
 //-----------------------------------------------------------------------------
 //methods
-τ.array.concat               = withType(κ(τ.array),  prim.array.concat           );
-τ.array.every                = withType(κ(τ.bool),   prim.array.every            );
-τ.array.filter               = withType(κ(τ.array),  prim.array.filter           );
-τ.array.forEach              = withType(κ(τ.any),    prim.array.forEach          );
-τ.array.indexOf              = withType(κ(τ.number), prim.array.indexOf          );
-τ.array.join                 = withType(κ(τ.string), prim.array.join             );
-τ.array.lastIndexOf          = withType(κ(τ.number), prim.array.lastIndexOf      );
-τ.array.$length              = withType(κ(τ.number), prim.array.$length          );
-τ.array.map                  = withType(κ(τ.array),  prim.array.map              );
-τ.array.pop                  = withType(κ(τ.any),    prim.array.pop              );
-τ.array.push                 = withType(κ(τ.array),  prim.array.push             );
-τ.array.reduce               = withType(κ(τ.any),    prim.array.reduce           );
-τ.array.reduceRight          = withType(κ(τ.any),    prim.array.reduceRight      );
-τ.array.reverse              = withType(κ(τ.array),  prim.array.reverse          );
-τ.array.shift                = withType(κ(τ.any),    prim.array.shift            );
-τ.array.slice                = withType(κ(τ.array),  prim.array.slice            );
-τ.array.some                 = withType(κ(τ.bool),   prim.array.some             );
-τ.array.sort                 = withType(κ(τ.array),  prim.array.sort             );
-τ.array.splice               = withType(κ(τ.array),  prim.array.splice           );
-τ.array.unshift              = withType(κ(τ.array),  prim.array.unshift          );
+τ.array.concat               = withType(τ.array,  prim.array.concat           );
+τ.array.every                = withType(τ.bool,   prim.array.every            );
+τ.array.filter               = withType(τ.array,  prim.array.filter           );
+τ.array.forEach              = withType(τ.any,    prim.array.forEach          );
+τ.array.indexOf              = withType(τ.number, prim.array.indexOf          );
+τ.array.join                 = withType(τ.string, prim.array.join             );
+τ.array.lastIndexOf          = withType(τ.number, prim.array.lastIndexOf      );
+τ.array.$length              = withType(τ.number, prim.array.$length          );
+τ.array.map                  = withType(τ.array,  prim.array.map              );
+τ.array.pop                  = withType(τ.any,    prim.array.pop              );
+τ.array.push                 = withType(τ.array,  prim.array.push             );
+τ.array.reduce               = withType(τ.any,    prim.array.reduce           );
+τ.array.reduceRight          = withType(τ.any,    prim.array.reduceRight      );
+τ.array.reverse              = withType(τ.array,  prim.array.reverse          );
+τ.array.shift                = withType(τ.any,    prim.array.shift            );
+τ.array.slice                = withType(τ.array,  prim.array.slice            );
+τ.array.some                 = withType(τ.bool,   prim.array.some             );
+τ.array.sort                 = withType(τ.array,  prim.array.sort             );
+τ.array.splice               = withType(τ.array,  prim.array.splice           );
+τ.array.unshift              = withType(τ.array,  prim.array.unshift          );
 τ.array.andThen              = τ.any.andThen;
 τ.array.toLocaleString       = τ.any.toLocaleString;
 τ.array.toString             = τ.any.toString;
@@ -159,9 +157,9 @@ var andThen = function(k){
 τ.bool.toLocaleString       = τ.any.toLocaleString;
 τ.bool.toString             = τ.any.toString;
 //operators
-τ.bool.not                  = withType(κ(τ.bool), prim.bool.not );
-τ.bool.or                   = withType(κ(τ.bool), prim.bool.or  );
-τ.bool.and                  = withType(κ(τ.bool), prim.bool.and );
+τ.bool.not                  = withType(τ.bool, prim.bool.not );
+τ.bool.or                   = withType(τ.bool, prim.bool.or  );
+τ.bool.and                  = withType(τ.bool, prim.bool.and );
 τ.bool.equalTo              = τ.any.equalTo;
 τ.bool.notEqualTo           = τ.any.notEqualTo;
 τ.bool.lessThan             = τ.any.lessThan;
@@ -175,17 +173,17 @@ var andThen = function(k){
 //-----------------------------------------------------------------------------
 //methods
 τ.number.andThen              = τ.any.andThen;
-τ.number.toExponential        = withType(κ(τ.number), prim.number.toExponential  );
-τ.number.toFixed              = withType(κ(τ.number), prim.number.toFixed        );
-τ.number.toPrecision          = withType(κ(τ.number), prim.number.toPrecision    );
+τ.number.toExponential        = withType(τ.number, prim.number.toExponential  );
+τ.number.toFixed              = withType(τ.number, prim.number.toFixed        );
+τ.number.toPrecision          = withType(τ.number, prim.number.toPrecision    );
 τ.number.toLocaleString       = τ.any.toLocaleString;
 τ.number.toString             = τ.any.toString;
 //operators
-τ.number.plus                 = withType(κ(τ.number), prim.number.plus  );
-τ.number.minus                = withType(κ(τ.number), prim.number.minus );
-τ.number.times                = withType(κ(τ.number), prim.number.times );
-τ.number.div                  = withType(κ(τ.number), prim.number.div   );
-τ.number.mod                  = withType(κ(τ.number), prim.number.mod   );
+τ.number.plus                 = withType(τ.number, prim.number.plus  );
+τ.number.minus                = withType(τ.number, prim.number.minus );
+τ.number.times                = withType(τ.number, prim.number.times );
+τ.number.div                  = withType(τ.number, prim.number.div   );
+τ.number.mod                  = withType(τ.number, prim.number.mod   );
 τ.number.equalTo              = τ.any.equalTo;
 τ.number.notEqualTo           = τ.any.notEqualTo;
 τ.number.lessThan             = τ.any.lessThan;
@@ -199,25 +197,25 @@ var andThen = function(k){
 //-----------------------------------------------------------------------------
 //methods
 τ.string.andThen              = τ.any.andThen;
-τ.string.charAt               = withType(κ(τ.string),  prim.string.charAt            );
-τ.string.charCodeAt           = withType(κ(τ.number),  prim.string.charCodeAt        );
-τ.string.concat               = withType(κ(τ.string),  prim.string.concat            );
-τ.string.indexOf              = withType(κ(τ.number),  prim.string.indexOf           );
-τ.string.lastIndexOf          = withType(κ(τ.number),  prim.string.lastIndexOf       );
-τ.string.$length              = withType(κ(τ.number),  prim.string.$length           );
-τ.string.localeCompare        = withType(κ(τ.number),  prim.string.localeCompare     );
-τ.string.match                = withType(κ(τ.array),   prim.string.match             );
-τ.string.replace              = withType(κ(τ.string),  prim.string.replace           );
-τ.string.search               = withType(κ(τ.number),  prim.string.search            );
-τ.string.slice                = withType(κ(τ.string),  prim.string.slice             );
-τ.string.split                = withType(κ(τ.array),   prim.string.split             );
-τ.string.substr               = withType(κ(τ.string),  prim.string.substr            );
-τ.string.substring            = withType(κ(τ.string),  prim.string.substring         );
-τ.string.toLocaleLowerCase    = withType(κ(τ.string),  prim.string.toLocaleLowerCase );
-τ.string.toLocaleUpperCase    = withType(κ(τ.string),  prim.string.toLocaleUpperCase );
-τ.string.toLowerCase          = withType(κ(τ.string),  prim.string.toLowerCase       );
-τ.string.toUpperCase          = withType(κ(τ.string),  prim.string.toUpperCase       );
-τ.string.trim                 = withType(κ(τ.string),  prim.string.trim              );
+τ.string.charAt               = withType(τ.string,  prim.string.charAt            );
+τ.string.charCodeAt           = withType(τ.number,  prim.string.charCodeAt        );
+τ.string.concat               = withType(τ.string,  prim.string.concat            );
+τ.string.indexOf              = withType(τ.number,  prim.string.indexOf           );
+τ.string.lastIndexOf          = withType(τ.number,  prim.string.lastIndexOf       );
+τ.string.$length              = withType(τ.number,  prim.string.$length           );
+τ.string.localeCompare        = withType(τ.number,  prim.string.localeCompare     );
+τ.string.match                = withType(τ.array,   prim.string.match             );
+τ.string.replace              = withType(τ.string,  prim.string.replace           );
+τ.string.search               = withType(τ.number,  prim.string.search            );
+τ.string.slice                = withType(τ.string,  prim.string.slice             );
+τ.string.split                = withType(τ.array,   prim.string.split             );
+τ.string.substr               = withType(τ.string,  prim.string.substr            );
+τ.string.substring            = withType(τ.string,  prim.string.substring         );
+τ.string.toLocaleLowerCase    = withType(τ.string,  prim.string.toLocaleLowerCase );
+τ.string.toLocaleUpperCase    = withType(τ.string,  prim.string.toLocaleUpperCase );
+τ.string.toLowerCase          = withType(τ.string,  prim.string.toLowerCase       );
+τ.string.toUpperCase          = withType(τ.string,  prim.string.toUpperCase       );
+τ.string.trim                 = withType(τ.string,  prim.string.trim              );
 τ.string.toLocaleString       = τ.any.toLocaleString;
 τ.string.toString             = τ.any.toString;
 //operators
